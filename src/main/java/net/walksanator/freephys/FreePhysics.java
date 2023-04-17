61d684cdf70c977edcdd5555d217cc2ac78a5895
walksanatora <walkerffo22@gmail.com>
2023-04-17 13:49:11 -0700

    is ... that it

---
+++ src/main/java/net/walksanator/freephys/FreePhysics.java
@@ -0,0 +1,35 @@
+package net.walksanator.freephys;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("freephys");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Congrats on the free physics mod 🙃")
+	}
+
+	public static Map<String, String> getParamsFromUrl(String url) throws Exception {
+		Map<String, String> params = new HashMap<>();
+		URL parsedUrl = new URL(url);
+		String query = parsedUrl.getQuery();
+		String[] pairs = query.split("&");
+		for (String pair : pairs) {
+			String[] parts = pair.split("=");
+			String key = URLDecoder.decode(parts[0], "UTF-8");
+			String value = URLDecoder.decode(parts[1], "UTF-8");
+			params.put(key, value);
+		}
+		return params;
+	}
+}
---
+++ src/main/java/net/walksanator/freephys/mixin/MixinHttpRequest.java
@@ -0,0 +1,60 @@
+package net.walksanator.freephys.mixin;
+
+import net.walksanator.freephys.ExampleMod;
+import net.diebuddies.util.HttpRequest;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(HttpRequest.class)
+public class MixinHttpRequest {
+	@Inject(at = @At("RETURN"), method = "get(Ljava/lang/String;)Ljava/lang/String;",cancellable = true)
+	private void phys$get(String urlToRead, CallbackInfo info) {
+		if (urlToRead.contains("verify.minecraftphysicsmod.com")) {
+			Map<String,String> opts = FreePhysics.getParamsFromUrl(urlToRead);
+			String code = opts.get("code");
+			if (code != null) {
+				info.cancel()
+				info.setReturnValue(code+"verified");
+				FreePhysics.LOGGER.info("Prevented another usless verification check");
+			}
+		}
+	}
+}
+/*
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example {
+    public static void main(String[] args) throws Exception {
+        String url = "http://verify.minecraftphysicsmod.com/verify?code=ash18239&name=walksanator";
+        Map<String, String> params = getParamsFromUrl(url);
+        String code = params.get("code");
+        String name = params.get("name");
+        System.out.println("code = " + code); // prints "code = ash18239"
+        System.out.println("name = " + name); // prints "name = walksanator"
+    }
+
+    private static Map<String, String> getParamsFromUrl(String url) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        URL parsedUrl = new URL(url);
+        String query = parsedUrl.getQuery();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            String[] parts = pair.split("=");
+            String key = URLDecoder.decode(parts[0], "UTF-8");
+            String value = URLDecoder.decode(parts[1], "UTF-8");
+            params.put(key, value);
+        }
+        return params;
+    }
+}
+
+ */
